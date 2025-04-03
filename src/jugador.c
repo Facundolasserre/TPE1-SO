33b32fb5b9e0c9407bae4c662d8357f57a79834d
Franco Ferrari <francoferraritt@gmail.com>
2025-04-03 12:27:16 -0300

    Basic development of player behaviour

--- src/jugador.c
+++ src/jugador.c
@@ -1,16 +1,60 @@
-#include "utils.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <semaphore.h>
+#include <stdbool.h>
+#include <time.h>
 
-//ESQUELETO
+#define SHM_STATE_NAME "/game_state"
+#define SHM_SYNC_NAME "/game_sync"
+#define MAX_INTENTOS 8
+
+typedef struct {
+    char nombre[16];
+    unsigned int puntaje;
+    unsigned int mov_invalidos;
+    unsigned int mov_validos;
+    unsigned short x, y;
+    pid_t pid;
+    bool bloqueado;
+} Jugador;
+
+typedef struct {
+    unsigned short ancho;
+    unsigned short alto;
+    unsigned int num_jugadores;
+    Jugador jugadores[9];
+    bool juego_terminado;
+    int tablero[];
+} EstadoJuego;
+
+typedef struct {
+    sem_t A, B, C, D, E;
+    unsigned int F;
+} Sincronizacion;
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
+
+
+const int movimientos[4] = {0, 2, 4, 6}; 
+
 int main(int argc, char *argv[]) {
-    // Tenemos que conectar a shmem y semaforos
-    while (!estado.juego_terminado) {
-        sem_wait(&sincronizacion.mutex_lectores);
-        // 
-        sem_post(&sincronizacion.mutex_lectores);
+    srand(time(NULL)); 
+    
+    
+    int ancho = atoi(argv[1]);
+    int alto = atoi(argv[2]);
 
-        // Calcular movimiento válido
-        unsigned char direccion = rand() % 8;
-        write(STDOUT_FILENO, &direccion, 1); // Enviar al pipe
+    while (1) {
+        int direccion = movimientos[rand() % 4]; 
+        write(STDOUT_FILENO, &direccion, 1);
+        usleep(100000); 
     }
+    
     return 0;
 }
--- src/semaphores.c
+++ src/semaphores.c
@@ -1,7 +1,7 @@
-#include "utils.h"
+/*#include "utils.h"
 
 void init_semaphores() {
     // Inicializar sem_t A, B, C, D, E
     sem_init(&sincronizacion.sem_vista, 1, 0);
     // ...
-}
+}*/
